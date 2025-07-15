c64f836cb284966d08f0fe747bec9b69e7a88ee2
Uday Harer <hareruday07@gmail.com>
2025-07-15 21:20:10 +0530

    Create main.py

---
+++ recommender/main.py
@@ -0,0 +1,13 @@
+with open("main.py", "w") as f: 
+  f.write(""" 
+from recommender.outfit_recommender import OutfitRecommender 
+
+def main(): 
+    recommender = OutfitRecommender("data/outfits.csv") 
+    print("Recommendations for color='Red', occasion='Party':") 
+    print(recommender.recommend(color='Red', occasion='Party')) 
+
+
+if __name__ == "__main__": 
+    main() 
+""") 
